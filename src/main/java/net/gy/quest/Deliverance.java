480ab18d79d85e257f345c2473ec0d274856e8ca
CinnamonRoll <grantyiu@proton.me>
2026-06-17 01:57:59 -0700

    added creative mode tab

--- src/main/java/net/gy/quest/Deliverance.java
+++ src/main/java/net/gy/quest/Deliverance.java
@@ -2,6 +2,7 @@ package net.gy.quest;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gy.quest.creativemodetab.ModCreativeModeTabs;
 import net.gy.quest.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,5 +16,6 @@ public class Deliverance implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 		ModItems.registerModItems();
+		ModCreativeModeTabs.registerModCreativeModeTabs();
 	}
 }
--- src/main/java/net/gy/quest/creativemodetab/ModCreativeModeTabs.java
+++ src/main/java/net/gy/quest/creativemodetab/ModCreativeModeTabs.java
@@ -1,4 +1,32 @@
 package net.gy.quest.creativemodetab;
 
+import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
+import net.gy.quest.Deliverance;
+import net.gy.quest.item.ModItems;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
 public class ModCreativeModeTabs {
+    public static final CreativeModeTab MOD_ITEM_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
+            Identifier.fromNamespaceAndPath(Deliverance.MOD_ID, "deliverance_items"),
+            FabricCreativeModeTab.builder().icon(() -> new ItemStack(ModItems.CURSED_BOOK))
+                    .title(Component.translatable("creativemodetab.quest.deliverance_items"))
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItems.COPPER_CORE);
+                        output.accept(ModItems.IRON_CORE);
+                        output.accept(ModItems.GOLD_CORE);
+                        output.accept(ModItems.DIAMOND_CORE);
+                        output.accept(ModItems.NETHERITE_CORE);
+                        output.accept(ModItems.SPIDER_CORE);
+                        output.accept(ModItems.CURSED_BOOK);
+                    }).build());
+
+
+    public static void registerModCreativeModeTabs() {
+        Deliverance.LOGGER.info("Registering Creative Mod Tabs for " + Deliverance.MOD_ID);
+    }
 }
